0dc14de79327565ddf1a39d92a1a391870463115
Safa C <csafa8001@gmail.com>
2022-09-13 21:42:25 +0530

    final

---
+++ article.js
@@ -0,0 +1,5 @@
+var button= document.getElementById("btn");
+button.addEventListener("click", showHidden);
+function showHidden(){
+document.getElementsByClassName('share')[0].style.display = 'block';  
+}
